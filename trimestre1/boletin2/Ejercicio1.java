33a5e4a6316f21c93a0cf2a867f5bf59be61de1e
Miguel <miguelopezrodriguez96@gmail.com>
2022-12-22 01:37:57 +0100

    se ha hecho una subida de prueba

---
+++ trimestre1/boletin2/Ejercicio1.java
@@ -0,0 +1,9 @@
+package trimestre1.boletin2;
+
+public class Ejercicio1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+    }
+    
+}
---
+++ trimestre1/boletin2/Ejercicio2.java
@@ -0,0 +1,13 @@
+package trimestre1.boletin2;
+
+import java.util.Scanner;
+
+public class Ejercicio2 {
+
+    private static Scanner teclado = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+    }
+
+}

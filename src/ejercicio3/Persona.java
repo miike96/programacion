c7ea85470ba1b22d5ac6c04279395e5ee47d5654
miike96 <miguelopezrodriguez96@gmail.com>
2023-02-14 17:48:27 +0100

    s

--- src/ejercicio3/Persona.java
+++ src/ejercicio3/Persona.java
@@ -1,43 +1,85 @@
 package ejercicio3;
 
-import java.time.LocalDate;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 public abstract class Persona {
 
     private String nombre;
-    private String textoMensaje;
-    private LocalDate fechaMensaje;
-
+    private String mensajeTexto;
+    private LocalDateTime fechaMensaje;
 
     /**
-     * Constructor de la clase Persona con parámetros
+     * Constructor con parametros de la clase padre en la que
+     * heredaran los hijos
+     * @param nombre
+     * @param mensajeTexto
+     * @param fechaMensaje
      */
 
-    public Persona(String nombre, String textoMensaje, LocalDate fechaMensaje) {
+    public Persona(String nombre, String mensajeTexto, LocalDateTime fechaMensaje) {
 
-        if (nombre == null || nombre.trim().length() == 0) {
-            throw new IllegalArgumentException("El nombre no puede ser nulo o vacío");
-        }
+        if(nombre == null || nombre.isEmpty())
+            throw new IllegalArgumentException("El nombre no puede ser nulo o vacio");
+
+        if(mensajeTexto == null || mensajeTexto.isEmpty())
+            throw new IllegalArgumentException("El mensaje no puede ser nulo o vacio");
+        
+        if(fechaMensaje == null)
+            throw new IllegalArgumentException("La fecha no puede ser nula");
 
         this.nombre = nombre;
-        this.textoMensaje = textoMensaje;
+        this.mensajeTexto = mensajeTexto;
         this.fechaMensaje = fechaMensaje;
     }
 
-    // Getters
+    //Getters
     public String getNombre() {
         return nombre;
     }
 
-    public String getTextoMensaje() {
-        return textoMensaje;
+    public String getMensajeTexto() {
+        return mensajeTexto;
     }
 
-    public LocalDate getFechaMensaje() {
+    public LocalDateTime getFechaMensaje() {
         return fechaMensaje;
     }
 
-    
+    /**
+     * Método para poder enviar un mensaje a otra persona. Recibirá como parámetro
+     * la persona a la que se le quiere enviar el mensaje y el mensaje a enviar.
+     * @param persona
+     * @param mensaje
+     */
 
-    
+    public void enviarMensaje(Persona persona, String mensaje) {
+
+        if(persona == null)
+            throw new IllegalArgumentException("La persona no puede ser nula");
+
+        if(mensaje == null || mensaje.isEmpty())
+            throw new IllegalArgumentException("El mensaje no puede ser nulo o vacio");
+
+        persona.recibirMensaje(mensaje);
+    }
+
+    /**
+     * Método para poder recibir un mensaje de otra persona. Recibirá como parámetro
+     * el mensaje que se le ha enviado.
+     * @param mensaje
+     */
+
+    public void recibirMensaje(String mensaje) {
+
+        if(mensaje == null || mensaje.isEmpty())
+            throw new IllegalArgumentException("El mensaje no puede ser nulo o vacio");
+
+        this.mensajeTexto = mensaje;
+        this.fechaMensaje = LocalDateTime.now();
+    }
 }
+
